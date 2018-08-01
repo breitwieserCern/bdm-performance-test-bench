4686dd21fd0cba8a273c4246f3c66744198fadc7
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2018-08-01 11:27:32 +0200

    Print parameter before running test bench

--- shortcuts.sh
+++ shortcuts.sh
@@ -1,6 +1,6 @@
 p="10000000 64 3 1 1000 0"
 alias c="g++ -Wall -std=c++17 -g -O3 -fopenmp -D_GLIBCXX_PARALLEL bdm-performance-test-bench.cc -o bdm-performance-test-bench"
-alias r='taskset -c 0-13,28-41 ./bdm-performance-test-bench $p'
+alias r='echo "Parameter: $p" && taskset -c 0-13,28-41 ./bdm-performance-test-bench $p'
 alias cr='c && r'
 alias t1="export OMP_NUM_THREADS=1"
 alias tall="unset OMP_NUM_THREADS"
